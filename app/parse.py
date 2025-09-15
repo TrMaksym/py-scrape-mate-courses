ec24a7cacaf01b4bacc40dbbdc45fa0ba320d226
Maximus Champion <maximuschampion2002@gmail.com>
2025-09-15 12:38:07 +0300

    refactoring

--- app/parse.py
+++ app/parse.py
@@ -4,8 +4,7 @@ import requests
 from typing import Optional
 from bs4 import BeautifulSoup, Tag
 
-BASE_URL ="https://mate.academy/"
-
+BASE_URL = "https://mate.academy/uk/courses"
 
 @dataclass
 class Course:
@@ -15,34 +14,21 @@ class Course:
     modules_count: Optional[int] = None
     topics_count: Optional[int] = None
 
-
 def parse_single_course(course_block: Tag) -> Course:
-    name = course_block.select_one(".course-card__title").get_text(strip=True)
-    short_description = course_block.select_one(".course-card__description").get_text(strip=True)
-    duration = course_block.select_one(".course-card__duration").get_text(strip=True)
+    name = course_block.select_one(".ProfessionCard_title__m7uno").get_text(strip=True)
+    short_description = course_block.select_one(".ProfessionCard_description__K8weo").get_text(strip=True)
+    duration = course_block.select_one(".ProfessionCard_duration__13PwX").get_text(strip=True)
 
-    modules = course_block.select(".course-card__modules")
-    topics = course_block.select(".course-card__topics")
-    modules_count = len(modules) if modules else None
-    topics_count = len(topics) if topics else None
-
-    return Course(name, short_description, duration, modules_count, topics_count)
-
+    return Course(name, short_description, duration, None, None)
 
 def get_all_courses() -> list[Course]:
     resp = requests.get(BASE_URL)
     resp.raise_for_status()
 
     soup = BeautifulSoup(resp.text, "html.parser")
-    course_elements = soup.select("li.FooterLinks_navListItem__GjxPp")
+    course_elements = soup.select(".ProfessionCard_cardWrapper__BCg0O")
 
-    courses = []
-    for el in course_elements:
-        name = el.get_text(strip=True)
-        short_description = ""
-        duration = ""
-        courses.append(Course(name, short_description, duration))
-
+    courses = [parse_single_course(el) for el in course_elements]
     return courses
 
 def write_courses_to_csv(courses: list[Course]) -> None:
@@ -57,6 +43,7 @@ def write_courses_to_csv(courses: list[Course]) -> None:
                 course.modules_count if course.modules_count is not None else "",
                 course.topics_count if course.topics_count is not None else ""
             ])
+
 if __name__ == "__main__":
     courses = get_all_courses()
     for course in courses:
